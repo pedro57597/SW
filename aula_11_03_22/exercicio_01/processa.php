a6690d95f83205310ee1af34343cdc8f9f2420cd
Pedro H <79934413+pedro57597@users.noreply.github.com>
2022-03-11 12:45:54 -0300

    Add files via upload

---
+++ aula_11_03_22/exercicio_01/processa.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <style>
+        p{
+            color:blue;
+        }
+    </style>
+</head>
+<body>
+    
+    <?php
+        $num1 = $_REQUEST['num1'];
+        $num2 = $_REQUEST['num2'];
+        $num3 = $_REQUEST['num3'];
+        $media = ($num1 + $num2 + $num3) /3;
+
+        $divisao = $soma
+
+        echo "<p>A soma é: $soma</p>";
+        echo "<p>A média é: $media</p>";
+
+        // pode ser tambem: (echo "A soma é: ".$soma;)
+
+    ?>
+
+</body>
+</html>
